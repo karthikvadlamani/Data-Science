1f94504170a2a229965bcde23d22ad3ceb5bf01b
karthikvadlamani <karthik.vadlamani.ix@gmail.com>
2017-08-05 15:23:55 -0700

    Add files via upload
    Twitter Sentiment Analysis with Python.

---
+++ TwitterStream.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from textblob import TextBlob
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import re
+import plotly
+import plotly.graph_objs as go
+import numpy as np
+
+
+compound = []
+count = 0
+
+ckey = 'k1mVlz2E9eXBkd2kG7gF0VgkJ'
+csecret = 'iEfbXbtmQ9UqeY2TFTg2q76VOZFucOH3kvYUq9IWWiDZedv7wW'
+
+atoken = '38171112-2rvUP6sSmqOQDIhwBcPcioPd1lYCc8Bpok71lTyPh'
+asecret = '7uLK9eg00O8bvcvTU66OheZCCRKofARzkE86HlHFr8LfR'
+
+
+
+class listener(StreamListener):
+    
+    def on_status(self, status):
+        global initime
+        
+        tweet = re.findall("[a-zA-Z]+|[.,!?;']", status.text, flags = re.MULTILINE)
+        s = " "
+        tweet = s.join(tweet)
+        blob = TextBlob(tweet)
+        
+        global compound
+        global count
+        global senti
+        count+= 1
+        senti = 0
+        for sen in blob.sentences:
+            senti+= sen.sentiment.polarity
+        compound.append(senti)
+
+        print(count)
+        print(tweet)
+        print(senti)
+        #print(compound)
+   
+        if count == 50:
+            return False
+        else:
+            return True
+        
+        
+    def on_error(self, status):
+        print(status)
+        
+    
+        
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener(count))
+twitterStream.filter(track = ["#incredibolt"])
+
+trace2 = go.Scatter(x = np.linspace(0, count, count), y = compound, mode = 'lines+markers', name = 'compound')
+plotly.offline.plot([trace2], filename='line-mode.html')
+            
+            
+            
